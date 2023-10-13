abd7531b306f39b5a615c178b2e7a4c56a8300b1
Gavrilenko Kirill <blackcourse@mail.ru>
2023-10-13 14:28:25 +0300

    jsom pars add

---
+++ src/test/java/testsJson.java
@@ -0,0 +1,30 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class testsJson {
+
+
+    @Test
+    public void testJsonParsing() {
+        Map<String, String> params = new HashMap<>();
+        params.put("message", "second message");
+        params.put ("timestamp", "2021-06-04 16:41:51");
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        ArrayList  messages = response.get("messages");
+        System.out.println(messages.get(1));
+
+
+    }
+}
